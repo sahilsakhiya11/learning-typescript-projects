ea4fb2dac43a54bd50ceaf27b137c366cd308bd3
Sahil Patel <sp0715007@gmail.com>
2024-01-18 22:54:02 -0500

    Solved firt syntatic sugar problem

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,20 @@
 // Put your announceMachines function here! ✨
 // See ./original.js for its older JavaScript code.
 
+const announceMachines = (announce, ...machines) => {
+	let label;
+	let labelsCount = 0;
+
+	for (let machine of machines) {
+		if (machine.label) {
+			label = machine.label;
+			labelsCount += 1;
+		} else {
+			label = `Make: ${machine.make}; Model: ${machine.model}`;
+		}
+		announce(label);
+	}
+	return labelsCount;
+};
+
 module.exports.announceMachines = announceMachines;
