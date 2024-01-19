f407af903a2fc3fdabd30163364e64c2243ae81e
Sahil Patel <sp0715007@gmail.com>
2024-01-18 23:33:34 -0500

    solution for callback to async await

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,20 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+async function checkEmotion(knownEmotions, emotion) {
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+	return knownEmotions.has(emotion);
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
